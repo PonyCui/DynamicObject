75f37f603e9f40fd0ea6f8ba0706977a0b5aed26
ponycui <cuis@vip.qq.com>
2016-05-10 23:17:27 +0800

    Initial Commit

---
+++ DynamicObject/DynamicObject.h
@@ -0,0 +1,19 @@
+//
+//  DynamicObject.h
+//  DynamicObject
+//
+//  Created by 崔 明辉 on 16/5/10.
+//  Copyright © 2016年 UED Center. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DynamicObject.
+FOUNDATION_EXPORT double DynamicObjectVersionNumber;
+
+//! Project version string for DynamicObject.
+FOUNDATION_EXPORT const unsigned char DynamicObjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DynamicObject/PublicHeader.h>
+
+
